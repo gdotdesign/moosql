44cc60ab98e0b528bee65f5af2b58480b5ff0598
Arian <arian@aryweb.nl>
2010-06-11 22:32:02 +0800

    Clean up some stuff Should work in 1.3b2 now remote error methods (they should not be methods of the class)

--- Source/moosql.js
+++ Source/moosql.js
@@ -16,63 +16,69 @@ provides: [MooSQL]
 
 ...
 */
-var MooSQL=new Class({
-  Implements:[Options,Events],
-  options:{
-    //Database name
-    dbName:'',
-    //Database version (max 4 numbers seperated by dots)
-    dbVersion:'',
-    //Database description (officially database display name)
-    dbDesc:'',
-    //Estimated size
-    dbSize:20*100
-  },
-  // Initialization
-  initialize:function(options){
-    //Set options
-    this.setOptions(options);
-    //Check if the browser supoorts SQL storage
-    if($defined(window.openDatabase)){
-      //Try to open the databse
-      this.db=window.openDatabase(this.options.dbName,this.options.dbVersion,this.options.dbDesc,this.options.dbSize,this.create.bindWithEvent(this));
-      if($defined(this.db)){
-	//If there is a database fire the 'databaseReady' event
-	this.fireEvent('databaseReady');
-      }
-    }else{
-      //If not supported fire the 'notSupported' event
-      this.fireEvent('notSupported');
-    }
-  },
-  create:function(db){
-    this.db=db;
-    //Database created so fire the 'databaseCreated' event
-    this.fireEvent('databaseCreated');
-  },
-  /**
-   * Exec function
-   * Executes an SQL statment on the database.
-   * 
-   * statement: any valid SQL statement...
-   * callback: function, runs it after the statement completed with 2 arguments: transaction, and the result.
-   * args: arguments to pass to the statement
-   **/
-  exec:function(statement,callback,args){
-    this.db.transaction(function(tr){
-      tr.executeSql(statement,$defined(args)?args:null,$defined(callback)?callback:$empty(),this.statmentError);
-    },this.error);
-  },
-  /**
-   * error callback: Fires the 'transactionError' event
-   **/
-  error:function(err){
-    this.fireEvent('transactionError',err);
-  },
-  /**
-   * statmentError callback: Fires the 'statmentError' event
-   **/
-  statmentError:function(err){
-    this.fireEvent('statmentError',err);
-  }
-  })
+var MooSQL = new Class({
+	
+	Implements : [Options, Events],
+	options: {
+		//Database name
+		dbName:'',
+		//Database version (max 4 numbers seperated by dots)
+		dbVersion:'',
+		//Database description (officially database display name)
+		dbDesc:'',
+		//Estimated size
+		dbSize:20*100
+	},
+	
+	initialize:function(options){
+		this.setOptions(options);
+
+		if(window.openDatabase != null){
+			
+			this.db = window.openDatabase(
+				this.options.dbName,
+				this.options.dbVersion,
+				this.options.dbDesc,
+				this.options.dbSize,
+				function(e){
+					this.create.apply(this,[e]);
+				}.bind(this)
+			);
+		}
+		
+		if (this.db != null) {
+			this.fireEvent('databaseReady');
+		} else {
+			this.fireEvent('notSupported');
+		}
+	},
+	
+	create: function(db){
+		this.db = db;
+		this.fireEvent('databaseCreated');
+	},
+	
+	/**
+	 * Exec function
+	 * Executes an SQL statment on the database.
+	 * 
+	 * statement: any valid SQL statement...
+	 * callback: function, runs it after the statement completed with 2 arguments: transaction, and the result.
+	 * args: arguments to pass to the statement
+	 **/
+	exec: function(statement, callback, args){
+		this.db.transaction(function(tr){
+			tr.executeSql(
+				statement, 
+				args,
+				callback || function(){},
+				function(err){
+					this.fireEvent('transactionError', err);
+				});
+		},
+		function(err){
+			this.fireEvent('statementError', err);
+		});
+	}
+	
+});
